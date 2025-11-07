66d62f91aca7d26be9571d056588dbca406a5801
emma-dev77 <emma.froidure@esme.fr>
2025-11-07 10:01:59 +0000

    partie 1

--- program.c
+++ program.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#define JEUX 6
+#define CARTES 52 * JEUX
+
+int sabot[CARTES];
+int position = 0;
+
+void initialiserSabot() {
+    int index=0;
+    for (int j = 0; j < JEUX; j++) {
+        for (int i = 1; i <= 13; i++) {
+            for (int k = 0; k < 4; k++){ 
+                sabot[index++] = i; 
+            }
+        }
+    }
+
 }
+
+void melangerSabot() {
+    for (int i = CARTES - 1; i > 0; i--) {
+        
+    }
+}
+
+
+
